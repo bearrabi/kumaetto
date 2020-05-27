2c61ae61c606eaf22b01f19923dcebda6ed9ad35
bearrabi <bearrabi0506@gmail.com>
2020-05-27 11:13:54 +0000

    create/view home.blade.php

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Kumaetto</title>
+</head>
+<body>
+  <h1>Kumaettoのホーム</h1>
+</body>
+</html>
