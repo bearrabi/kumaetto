3bb0a3aafe089486c4af6eb3a9c331f2809ae921
bearrabi <bearrabi0506@gmail.com>
2020-05-27 11:50:49 +0000

    edit/view home.blade.php for change to original home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,13 @@
-@extends('layouts.app')
-
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
-
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-@endsection
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="{{ asset('css/home.css') }}">
+  <title>Kumaetto</title>
+</head>
+<body>
+  <h1>Kumaettoのホーム</h1>
+  
+</body>
+</html>
