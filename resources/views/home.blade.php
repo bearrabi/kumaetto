69837b5887a83c60b4cebc534b9229d6bc900063
bearrabi <bearrabi0506@gmail.com>
2020-05-28 10:58:46 +0000

    edit/view home.blade.php for add slide base

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,11 +19,37 @@
 </head>
 <body>
   <div class="container">
+    
+    <!--タイトル-->
     <div id="title_row" class="row">
       <h1 id="title">Kumaetto</h1>
     </div>
-  </div>
-  
 
+    <!--メイン画像のスライドショー-->
+    <div class="row">
+      <div id="carouselControls-anime" class="carousel slide" data-ride="false">
+
+        <!-- ページャー部分 -->
+        <ol class="carousel-indicators">
+          <li data-target="#carouselControls-anime" data-slide-to="0" class="active"></li>
+          <li data-target="#carouselControls-anime" data-slide-to="1"></li>
+          <li data-target="#carouselControls-anime" data-slide-to="2"></li>
+        </ol>
+
+        <!--スライド部分-->
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <div class="slide_item" id="item1"></div>
+          </div>
+          <div class="carousel-item">
+            <div class="slide_item" id="item2"></div>        
+          </div>
+          <div class="carousel-item" >
+            <div class="slide_item" id="item3"></div>
+          </div>
+        </div>
+
+      </div>
+    </div>
 </body>
 </html>
