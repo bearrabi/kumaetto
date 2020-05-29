ea4be7be9b5802c95391776fb8ecec13fd890e35
bearrabi <bearrabi0506@gmail.com>
2020-05-30 08:06:34 +0900

    edit/route web.php for check process login to show mypage

--- routes/web.php
+++ routes/web.php
@@ -13,7 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {   return view('home');    });
+Route::get('/', function () {   return view('home');      });
+Route::get('/mypage', function(){ return view('mypage');  });
 
 Auth::routes();
 
