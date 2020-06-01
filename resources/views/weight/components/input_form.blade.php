b47bc20e748d1c9096ce6ff01b2d40ef5f5269cc
bearrabi <bearrabi0506@gmail.com>
2020-06-02 06:44:12 +0900

    edit/component input_form.blade.php for test display form

--- resources/views/weight/components/input_form.blade.php
+++ resources/views/weight/components/input_form.blade.php
@@ -1,1 +1,31 @@
-<h1>weight input form</h1>
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-header">{{ __('Login') }}</div>
+
+          <div class="card-body">
+              <form method="POST" action="{{ route('weight.store') }}">
+                  @csrf
+
+                  <div class="form-group row">
+                      <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
+
+                      <div class="col-md-6">
+                          <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
+
+                          @error('name')
+                              <span class="invalid-feedback" role="alert">
+                                  <strong>{{ $message }}</strong>
+                              </span>
+                          @enderror
+                      </div>
+                  </div>
+
+                  
+              </form>
+          </div>
+        </div>
+    </div>
+  </div>
+</div>-->
