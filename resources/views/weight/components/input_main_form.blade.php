a872b72cc20a25940e694b06b41f04becb05e393
bearrabi <bearrabi0506@gmail.com>
2020-06-03 20:21:51 +0900

    create/component input_main_form.blade.php for enable to input weight info

---
+++ resources/views/weight/components/input_main_form.blade.php
@@ -0,0 +1,7 @@
+<div class="form-group">
+  <label for="{{$contents_name}}1">{{$label_name}}</label>
+</div>
+<div class="form-inline">
+  <input name="{{$contents_name}}1" type="text" class="form-control txtbx" id="{{$contents_name}}1"><span id="dot">.</span>
+  <input name="{{$contents_name}}2" type="text" class="form-control txtbx" id="{{$contents_name}}2"><span id="unit">{{$unit}}</span>
+</div>
