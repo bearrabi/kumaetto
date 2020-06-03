5d113aaa44bdb4cc91769951ee9066f342150318
bearrabi <bearrabi0506@gmail.com>
2020-06-03 21:05:14 +0900

    create/migration create_weights_table.php for define weights table

---
+++ database/migrations/2020_06_03_205828_create_weights_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateWeightsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('weights', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('user_id');
+            $table->double('weight',4,1);
+            $table->dateTime('measured_dt');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('weights');
+    }
+}
