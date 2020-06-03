d55bde3f4837aa77aaddb6d8fafd7f760ab5f517
bearrabi <bearrabi0506@gmail.com>
2020-06-03 19:58:02 +0900

    edit/component input_form_blade.php for enable to input date and time

--- resources/views/weight/components/input_form.blade.php
+++ resources/views/weight/components/input_form.blade.php
@@ -2,15 +2,23 @@
   <div class="row justify-content-center">
     <div class="col-md-8">
       <div class="card">
-        <div class="card-header">{{ __('Login') }}</div>
+        <div class="card-header">{{ __('体重記録') }}</div>
 
           <div class="card-body">
               <form method="POST" action="{{ route('weight.store') }}">
                   @csrf
 
                   <!--日付--->
-                  @component('weight.components.input_date_form',['years' => $years, 'months' => $months, 'days' => $days])   @endcomponent
+                  @component('weight.components.input_date_form',['years' => $years, 
+                                                                  'months' => $months, 
+                                                                  'days' => $days])   
+                  @endcomponent
 
+                  <!--時刻-->
+                  @component('weight.components.input_time_form',['hours' => $hours, 
+                                                                  'minutes' => $minutes, 
+                                                                  'seconds' => $seconds])
+                  @endcomponent
                   
               </form>
           </div>
