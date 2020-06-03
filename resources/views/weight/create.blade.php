39f5acc0ec524e623abca0feb65fb6251e95edb1
bearrabi <bearrabi0506@gmail.com>
2020-06-03 20:47:58 +0900

    edit/view create.blade.php for select readonly or normal

--- resources/views/weight/create.blade.php
+++ resources/views/weight/create.blade.php
@@ -4,7 +4,10 @@
 @section('content')
 
 <!--入力フォーム-->
-@component('weight.components.input_form',[ 'route' => 'weight.store',
+@component('weight.components.input_form',[ 'date_input_state' => 'normal',
+                                            'time_input_state' => 'normal',
+                                            'main_input_state' => 'normal',
+                                            'route' => 'weight.store',
                                             'button_value' => '保存',
                                             'years' => $years, 
                                             'months' => $months, 
