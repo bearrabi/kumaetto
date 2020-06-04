de25c704de7749940460df4150f2f24e30a55b00
bearrabi <bearrabi0506@gmail.com>
2020-06-05 06:10:59 +0900

    edit/component input_main_form.blade.php for set defalt contents values

--- resources/views/weight/components/input_main_form.blade.php
+++ resources/views/weight/components/input_main_form.blade.php
@@ -2,6 +2,11 @@
   <label for="{{$contents_name}}1">{{$label_name}}</label>
 </div>
 <div class="form-inline">
+@if ($contents_values == NULL)
   <input name="{{$contents_name}}1" type="text" class="form-control txtbx" id="{{$contents_name}}1"><span id="dot">.</span>
   <input name="{{$contents_name}}2" type="text" class="form-control txtbx" id="{{$contents_name}}2"><span id="unit">{{$unit}}</span>
+@else
+  <input name="{{$contents_name}}1" type="text" class="form-control txtbx" id="{{$contents_name}}1" value="{{$contents_values[0]}}"><span id="dot">.</span>
+  <input name="{{$contents_name}}2" type="text" class="form-control txtbx" id="{{$contents_name}}2" value="{{$contents_values[1]}}"><span id="unit">{{$unit}}</span>
+@endif
 </div>
