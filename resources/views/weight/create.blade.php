26f921669be6695a9384ed98d12d13f2bc7f4634
bearrabi <bearrabi0506@gmail.com>
2020-06-05 06:13:27 +0900

    edit/view create.blade.php for set default contents values of empty

--- resources/views/weight/create.blade.php
+++ resources/views/weight/create.blade.php
@@ -2,20 +2,45 @@
 @include('layouts.sections.header')
 @include('layouts.sections.navbar')
 @section('content')
+<div class="container">
+  <div class="row justify-content-center">
+    <div class="col-md-8">
+      <div class="card">
+        <div class="card-header">{{ __('体重記録') }}</div>
 
-<!--入力フォーム-->
-@component('weight.components.input_form',[ 'date_input_state' => 'normal',
-                                            'time_input_state' => 'normal',
-                                            'main_input_state' => 'normal',
-                                            'route' => 'weight.store',
-                                            'button_value' => '保存',
-                                            'years' => $years, 
-                                            'months' => $months, 
-                                            'days' => $days,
-                                            'hours' => $hours,
-                                            'minutes' => $minutes,
-                                            'seconds' => $seconds]
-                                            )  
-@endcomponent
+          <div class="card-body">
+              <form method="POST" action="{{ route('weight.store') }}">
+                  @csrf
 
+                  <!--日付--->
+                  @component('weight.components.input_date_form',['input_state' => 'normal',
+                                                                  'years' => $years, 
+                                                                  'months' => $months, 
+                                                                  'days' => $days])   
+                  @endcomponent
+
+                  <!--時刻-->
+                  @component('weight.components.input_time_form',['input_state' => 'normal',
+                                                                  'hours' => $hours, 
+                                                                  'minutes' => $minutes, 
+                                                                  'seconds' => $seconds])
+                  @endcomponent
+                  
+                  <!--体重-->
+                  @component('weight.components.input_main_form',['input_state' => 'normal',
+                                                                  'contents_name' => 'weight', 
+                                                                  'label_name' => '体重', 
+                                                                  'unit' => 'Kg',
+                                                                  'contents_values' => ''])  <!--新規登録なので、入力項目の初期値(contents_values)は空とする-->
+
+                  @endcomponent
+                  
+                  <!--登録ボタン-->
+                  <input class="btn btn-info" type="submit" value="保存">
+              </form>
+          </div>
+        </div>
+    </div>
+  </div>
+</div>
 @endsection
