a2644619e2f54a1687fbe394ca26eb661c013a63
bearrabi <bearrabi0506@gmail.com>
2020-06-06 06:06:14 +0900

    edit/view mypage.blade.php for add create button

--- resources/views/mypage.blade.php
+++ resources/views/mypage.blade.php
@@ -5,7 +5,23 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            @component('layouts.components.slide')  @endcomponent
+
+            <!--スライド部分-->
+            @component('layouts.components.slide' [ 'slide1_path'   =>  '',
+                                                    'slide1_param'  =>  '',
+                                                    'slide2_path'   =>  '',
+                                                    'slide2_param'  =>  '',
+                                                    'slide3_path'   =>  '',
+                                                    'slide3_param'  =>  ''
+                                                    ])  
+            @endcomponent
+
+            <!--ログインボタン、登録ボタン-->
+            <div class="row justify-content-md-center">
+                <div id="login_btn" class="home_buttons col-lg-3 col-md-3 col-sm-3"> 
+                    <a class="btn btn-primary" href="{{ route('weight.create') }}">新規登録</a>
+                </div>
+            </div>
         </div>
     </div>
 </div>
